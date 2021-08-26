a9f9d5faf5cde538bdd98acc7015c5eef7054bcc
fora-heims <foraheims@gmail.com>
2021-08-26 16:30:59 -0700

    Created UI for magic-eight-ball and button listener

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // import functions
 
 // reference needed DOM elements
-
+const input = document.getElementById('question-input');
+const button = document.getElementById('submit-button');
+const answer = document.getElementById('answer-section');
 
 // set event listeners 
     // get user input(s)
+button.addEventListener('click', () => {
+    console.log('Button clicked')
+})
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
